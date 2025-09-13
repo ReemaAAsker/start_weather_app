40405c9a0dc7e8f79591dde3c3994a5eaaf62da3
ReemaAAsker <reemaaliasker@gmail.com>
2025-09-13 13:53:44 +0200

    chnage style for button in the starter template

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(home: LoadingScreen()));
+}
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -9,16 +14,45 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void getLocation() {
+    // try {
+    //   Position p = await Geolocator.getCurrentPosition();
+    //   print(p);
+    // } catch (e) {
+    //   print(e);
+    // }
+
+    Geolocator.getCurrentPosition()
+        .then((position) {
+          print(position);
+        })
+        .catchError((e) {
+          print(e);
+        });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            //TODO:get current location
+            getLocation();
           },
-          child: Text("Get Location"),
+          child: const Text("Get Location"),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+          ),
         ),
       ),
     );
   }
 }
+/*
+
+Future >> data , error
+
+
+
+
+*/
